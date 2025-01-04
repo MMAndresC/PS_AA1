489d4849bd2490decddd3b3f02d08ebff3ea5d02
Mar <mandrescasabon@hotmail.com>
2025-01-04 19:38:31 +0100

    :tada: init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.svalero.ps_aa1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.svalero.ps_aa1 to javafx.fxml;
+    exports com.svalero.ps_aa1;
+}
