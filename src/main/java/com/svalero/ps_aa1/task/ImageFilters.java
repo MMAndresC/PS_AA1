bc35910ba97c5062d2be00dcef22cb2e00da55eb
Mar <mandrescasabon@hotmail.com>
2025-01-04 22:00:48 +0100

    :sparkles: add class with image filters

---
+++ src/main/java/com/svalero/ps_aa1/task/ImageFilters.java
@@ -0,0 +1,75 @@
+package com.svalero.ps_aa1.task;
+import java.awt.image.BufferedImage;
+
+public class ImageFilters {
+    private int adjustRange(int value) {
+        return (Math.min(Math.max(0, value), 255));
+    }
+    public BufferedImage changeBrightness(int brightnessValue, BufferedImage image){
+        int width = image.getWidth();
+        int height = image.getHeight();
+        for (int i = 0; i < width; i++) {
+
+            for (int j = 0; j < height; j++) {
+
+                int[] rgb = image.getRaster().getPixel(i, j, new int[3]);
+
+                int red = adjustRange(rgb[0] + brightnessValue);
+                int green = adjustRange(rgb[1] + brightnessValue);
+                int blue = adjustRange(rgb[2] + brightnessValue);
+
+                int[] newRgb = { red, green, blue };
+
+                image.getRaster().setPixel(i, j, newRgb);
+            }
+        }
+        return image;
+    }
+
+    public BufferedImage toGrayScale(BufferedImage image){
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        for (int i = 0; i < width; i++) {
+
+            for (int j = 0; j < height; j++) {
+
+                int[] rgb = image.getRaster().getPixel(i, j, new int[3]);
+
+                int red = rgb[0];
+                int green = rgb[1];
+                int blue = rgb[2];
+
+                int avg = (red + green + blue) / 3;
+
+                int[] newRgb = { avg, avg, avg };
+
+                image.getRaster().setPixel(i, j, newRgb);
+            }
+        }
+        return image;
+    }
+
+    public BufferedImage invertColor(BufferedImage image){
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        for (int i = 0; i < width; i++) {
+
+            for (int j = 0; j < height; j++) {
+
+                int[] rgb = image.getRaster().getPixel(i, j, new int[3]);
+
+                int red = 255 - rgb[0];
+                int green = 255 - rgb[1];
+                int blue = 255 - rgb[2];
+
+                int[] newRgb = { red, green, blue };
+
+                image.getRaster().setPixel(i, j, newRgb);
+            }
+        }
+        return image;
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.svalero.ps_aa1 {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens com.svalero.ps_aa1 to javafx.fxml;
     exports com.svalero.ps_aa1;
