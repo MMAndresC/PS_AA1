bc8e5af7c3556a2275cfbdf994299ff5b0a41f26
Mar <mandrescasabon@hotmail.com>
2025-01-05 17:17:53 +0100

    :coffin: comment example file

--- src/main/java/com/svalero/ps_aa1/HelloApplication.java
+++ src/main/java/com/svalero/ps_aa1/HelloApplication.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/*
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
@@ -20,4 +21,4 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
-}
+}*/
