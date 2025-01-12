454ca9ccfe6927b0b066fb3ddd6d930a407accc1
Mar <mandrescasabon@hotmail.com>
2025-01-12 14:22:04 +0100

    :wrench: config to add library

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.svalero.ps_aa1 {
 
     requires org.kordamp.bootstrapfx.core;
     requires java.desktop;
+    requires javafx.swing;
 
     opens com.svalero.ps_aa1 to javafx.fxml;
     exports com.svalero.ps_aa1;
